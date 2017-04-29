afd71d8df02b81246f67e4dc4216d94b781897dc
Jake <jakehallam95@gmail.com>
2017-04-29 13:06:19 +0100

    updates happened

---
+++ Project Files/include/constants.h
@@ -0,0 +1,15 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+typedef int BOOL;
+
+#define TRUE 1
+#define FALSE 0
+
+#define ROW 1
+#define COLUMN 2
+#define CHAMBER 3
+#define CELL 4
+
+#endif
+
---
+++ Project Files/test/grid_tests.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <minunit.h>
+#include "../include/grid.h"
+
+int main(){
+
+	puts("Success!");
+	return 0;
+}
