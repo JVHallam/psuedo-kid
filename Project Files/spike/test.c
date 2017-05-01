2391b6d32095dda6df4c3ce5b8253de4518d478d
Jake <jakehallam95@gmail.com>
2017-05-01 20:32:05 +0100

    Added some tests for the file parser. Also added some test puzzles for the parser to pass and fail on

--- Project Files/spike/test.c
+++ Project Files/spike/test.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct myTest{
 	int a;
 }superFreak;
 
-int main(){
+static char* string_space(char* target_string){
+	char* space = malloc(strlen(target_string) + 1);
+	strcpy(space, target_string);
+	return space;
+}
 
-	puts("It compiles!");
+int main(){
+	char* a = "This is a test";
+	char* b = "this is a test";
+
+	char* arr[] = {a, b};
+
+	char** c = arr;
+	for(int i = 0; i < 2; ++i){
+		printf("%s\n", *(c++));	
+	}
+
+	char* test[] = {
+		"This is a test",
+		"This is a test"
+	};
+
+	puts("Space assigned");
+
+	c = test;
+	for(int i = 0; i < 2; ++i){
+		printf("%s\n", *(c++));	
+	}
+
 
 	return 0;
 }
