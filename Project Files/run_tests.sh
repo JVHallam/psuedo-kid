63e39db9b01dc3cec80ec0119a098d315b777c7f
Jake <jakehallam95@gmail.com>
2017-05-09 20:36:06 +0100

    Added in more tests for logic.c, also adapted ./run_tests.sh to surpress all output from the tests and only output which failed, if any

--- Project Files/run_tests.sh
+++ Project Files/run_tests.sh
@@ -1,6 +1,51 @@
 #!/bin/bash
+#Global Variables:
+declare -i failed_tests_total=0;
+console_output_file="console_output.txt";
+failed_tests_file="failed_tests.txt";
 
-make tests;
+#Functions
+function failed_test {
+	echo " > $1" >> $failed_tests_file;
+	((failed_tests_total++));
+}
 
-make traversal_tests;
+function run_test {
+	make $1 &>> $console_output_file;
 
+	if [ $? != 0 ]
+	then
+		failed_test $1;
+	fi
+}
+
+#Main:
+
+#Set up the 2 output files:
+if [ -e $console_output_file ]
+then
+	rm $console_output_file;
+fi
+
+if [ -e $failed_tests_file ]
+then 
+	rm $failed_tests_file;
+fi
+touch $failed_tests_file;
+
+#Declare and run tests:
+test_list=( "grid_test" "traversal_test" "logic_test" );
+for test in "${test_list[@]}"
+do
+	run_test $test;
+done
+
+#If any failed
+if [ $failed_tests_total != 0 ]
+then
+	echo "Number of failed tests = $failed_tests_total";
+	echo "Tests that failed:";
+	cat failed_tests.txt;
+else
+	echo "All tests completed without errors!";
+fi
