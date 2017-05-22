2c7ff3716ce4eacbf26e91cabb7c0c7227fc8f7e
Jake <jakehallam95@gmail.com>
2017-05-22 22:58:46 +0100

    Began work on porcelain commands

---
+++ Project Files/include/porcelain.h
@@ -0,0 +1,32 @@
+#ifndef PORCELAIN_H_
+#define PORCELAIN_H_
+
+#include "../include/grid.h"
+/*
+	The point of porcelain is to do all of the table management and correspond with the
+	following algorithms file.
+
+	The most basic of basic functions:
+		porcelain.c should not be returning ANY pointers
+
+		added_to_tables(file_name_that_becomes_key)
+
+		int get_value(cell index, key)
+
+		BOOL set_value(cell_index, key)
+			Then goes and updates all other values
+
+		BOOL is_present(area_index)
+
+	Whenever a change is made, porcelain should set all of the valid values of cells that are
+	effected.
+*/
+
+//Adds the puzzle from the file to the grid.
+//file_name then becomes the key to access that puzzle, via the other commands.
+BOOL new_puzzle(char* file_name);
+
+//Cleans up the table once finished.
+void porcelain_cleanup();
+
+#endif
---
+++ Project Files/test/porcelain_test.c
@@ -0,0 +1,39 @@
+#include "../include/porcelain.h"
+#include "../include/minunit.h"
+#include "../include/stdio.h"
+
+int tests_run = 0;
+
+static char* run_all_tests();
+
+
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+int main(){
+	char* result = run_all_tests();
+
+	if(result){
+		printf("\n%s\n\n", result);
+	}
+
+	return result != 0;
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+static char* test_new_puzzle(){
+	char* test_puzzle = "test/resources/valid1.puzzle";
+	BOOL is_puzzle_added = new_puzzle(test_puzzle);
+
+	mu_assert(\
+		"A valid puzzle was not added.",\
+		is_puzzle_added == TRUE
+	);
+
+	return 0;
+}
+
+static char* run_all_tests(){
+
+	mu_run_test(test_new_puzzle);
+
+	return 0;
+}
